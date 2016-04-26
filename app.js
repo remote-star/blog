a58ef54fdf5aca8347834d8d962df1c700a67f86
remote-star <148054685@qq.com>
2016-04-26 14:38:53 +0800

    Add a favicon

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
